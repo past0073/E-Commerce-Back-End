2cc26a69dc82ebd73a609ce58bb7db12a10c4808
AP <past0073@umn.edu>
2021-03-20 19:29:51 -0500

    Removed excess comments.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product}]
@@ -18,7 +17,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product}]
@@ -37,7 +35,6 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
-    console.log(categoryData);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
